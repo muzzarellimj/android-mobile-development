7531fd1f8220274b9086a7ee91fa0846c8e2875f
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-02-08 22:09:48 -0500

    Create MainActivity class and layout
    MainActivity.kt is the class for the activity that will be initialised on app launch, and activity_main.xml is the corresponding layout file. This activity has been added to the AndroidManifest.xml and set to be the main action on launch.

---
+++ assignment/assignment-2/app/src/main/java/dev/muzzarelli/android/recycler/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.muzzarelli.android.recycler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.muzzarellimj.android.recycler.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
