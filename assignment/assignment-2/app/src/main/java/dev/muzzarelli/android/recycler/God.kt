49616e63d926fe1757e103d1d9863a1a0b682d9e
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-02-08 22:46:28 -0500

    Create God data class and sample JSON set

---
+++ assignment/assignment-2/app/src/main/java/dev/muzzarelli/android/recycler/God.kt
@@ -0,0 +1,18 @@
+package dev.muzzarelli.android.recycler
+
+/**
+ * A data class to represent ascendant characters -- deities -- in RuneScape.
+ *
+ * @property id a unique numeric identifier
+ * @property name a character name
+ * @property tier a loose rank -- a value dependent on general power and abilities
+ * @property race a loose category the character fits within
+ * @property description a short character description -- an examine message
+ */
+data class God(
+    val id: Int,
+    val name: String,
+    val tier: String,
+    val race: String,
+    val description: String
+)
