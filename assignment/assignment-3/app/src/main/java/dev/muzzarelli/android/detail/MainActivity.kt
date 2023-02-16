56159fb8589d72eb06c7ad69888efe950369dcc4
Michael Muzzarelli <muzzarellimj@outlook.com>
2023-02-16 13:19:25 -0500

    Create PokeApiClient with OkHttpClient
    PokeApiClient utilises OkHttpClient to make requests to web REST API with two functions: (1) requestRepository requests a list of all available Pokemon identified by name and URL and (2) requestPokemon requests a particular Pokemon identified by a number of attributes to be later parsed. Android permission INTERNET has been enabled in the manifest.

--- assignment/assignment-3/app/src/main/java/dev/muzzarelli/android/detail/MainActivity.kt
+++ assignment/assignment-3/app/src/main/java/dev/muzzarelli/android/detail/MainActivity.kt
@@ -2,11 +2,15 @@ package dev.muzzarelli.android.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.muzzarelli.android.detail.request.PokeApiClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pokeApiClient = PokeApiClient()
+        pokeApiClient.requestRepository(10, 0)
     }
 }
